8780da7af2130a43027c97caba4199832e9139d8
zawakichi_ <zawakichi21@gmail.com>
2019-12-31 15:08:29 +0900

    init

---
+++ CMakeFiles/Template-Matching.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Template-Matching.dir/src/Template.cpp.o"
+  "Template-Matching"
+  "Template-Matching.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Template-Matching.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
